c13c520dfc7aef2d932702d3ccb2820c3c27c027
Yohann29 <yohann.vernet29@gmail.com>
2017-12-07 18:54:29 +0100

    Affichage du score et modification de quelques couleurs + gestion des déplacements

--- src/snake/Character.java
+++ src/snake/Character.java
@@ -1,21 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package snake;
 
-import java.awt.Color;
-import java.awt.Graphics;
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 import javax.swing.*;
 
-/**
- *
- * @author Amaury
- */
-public class Character extends JPanel{
+public class Character extends JPanel {
+
     public int size;
     Graphics g;
     public boolean play = true;
@@ -24,90 +15,146 @@ public class Character extends JPanel{
     ArrayList<Ring> body = new ArrayList<Ring>();
     ArrayList<Enemy> enemies = new ArrayList<Enemy>();
     public int direction = 4;
-    public Character(Graphics g, int size){
+
+    public Character(Graphics g, int size) {
         this.g = g;
         this.size = size;
         sc = new Scanner(System.in);
     }
-    
-    public void play(){
+
+    public void play() {
         createCharacter();
-        while(play == true){
-            g.setColor(Color.LIGHT_GRAY);
-            g.fillOval(0,0,Window.width,Window.height);
-            
+        while (play == true) {
+
+            g.setColor(Color.DARK_GRAY);
+            g.fillRect(0, 0, Window.width, Window.height);
+
             createEnemy();
             drawEnemy();
             drawCharacter();
-            
+
+            sleep(100);
+
+            move();
+            checkCollision();
+
         }
-        
+
     }
-    
-    public void createEnemy(){
+
+    public void sleep(int time) {
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException ex) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    public void createEnemy() {
         int randX, randY;
         Boolean creation = true;
-        
-        while(enemies.size() < numberOfEnemies){
-            int WindowWidth = ((Window.width-20)/10)-2;
-            int WindowHeight = ((Window.height-20)/10)-2;
-            
-            randX = (int)(Math.random()*(WindowWidth))+3;
-            randY = (int)(Math.random()*(WindowHeight))+3;
-            
-            randX = (randX*10);
-            randY = (randY*10);
-            
-            for(int i = 0 ; i < body.size() ; i++){
+
+        while (enemies.size() < numberOfEnemies) {
+            int WindowWidth = ((Window.width - 20) / 10) - 2;
+            int WindowHeight = ((Window.height - 20) / 10) - 2;
+
+            randX = (int) (Math.random() * (WindowWidth)) + 3;
+            randY = (int) (Math.random() * (WindowHeight)) + 3;
+
+            randX = (randX * 10);
+            randY = (randY * 10);
+
+            for (int i = 0; i < body.size(); i++) {
                 Ring ifExist = body.get(i);
-                if(randX == ifExist.posX && randY == ifExist.posY){
+                if (randX == ifExist.posX && randY == ifExist.posY) {
                     creation = false;
                 }
             }
-            
-            if(creation == true){
+
+            if (creation == true) {
                 enemies.add(new Enemy(randX, randY, Color.GREEN));
             }
         }
-        
+
     }
-    
-    public void createCharacter(){
-        for(int j = 0 ; j < this.size ; j++){
-            int height; 
-            height = ((int) Window.height/2)/10;
+
+    public void createCharacter() {
+        for (int j = 0; j < this.size; j++) {
+            int height;
+            height = ((int) Window.height / 2) / 10;
             height *= 10;
-            if(j == 0){
-                body.add(new Ring(Window.width/2+((j)*10),height,Color.RED));
-            } else if(j > 0){
-                body.add(new Ring(Window.width/2+((j)*10),height,Color.BLACK));
+            if (j == 0) {
+                body.add(new Ring(Window.width / 2 + ((j) * 10), height, Color.RED));
+            } else if (j > 0) {
+                body.add(new Ring(Window.width / 2 + ((j) * 10), height, Color.WHITE));
             }
         }
     }
-    
-    public void drawEnemy(){
-        for(int x = 0 ; x < enemies.size() ; x++){
+
+    public void drawEnemy() {
+        for (int x = 0; x < enemies.size(); x++) {
             Enemy e = enemies.get(x);
             g.setColor(e.color);
-            g.fillOval(e.posX,e.posY,10,10);
+            g.fillOval(e.posX, e.posY, 10, 10);
         }
     }
-    
-    public void drawCharacter(){
-        for(int i=0 ; i < body.size() ; i++){
+
+    public void showScore() {
+        g.setFont(new Font("Calibri", Font.PLAIN, 15));
+        g.drawString(Integer.toString(body.size()), 10, Window.height - 10);
+    }
+
+    public void drawCharacter() {
+        for (int i = 0; i < body.size(); i++) {
             Ring r;
             r = body.get(i);
             g.setColor(r.color);
-            g.fillOval(r.posX,r.posY,10,10);
+            g.fillOval(r.posX, r.posY, 10, 10);
+            showScore();
         }
-        
     }
-    
-    public void move(){
+
+    public void checkCollision() {
+        for (int i = 0; i < enemies.size(); i++) {
+            Enemy checkEnemy = enemies.get(i);
+            Ring checkCharacter = body.get(0);
+            Ring lastPosition = body.get(body.size() - 1);
+
+            if (checkEnemy.posX == checkCharacter.posX && checkEnemy.posY == checkCharacter.posY) {
+                enemies.remove(i);
+                body.add(new Ring(200 + ((lastPosition.posX) + 10), 0, Color.WHITE));
+            }
+        }
+
+        for (int i = 1; i < body.size(); i++) {
+            Ring characterBody = body.get(i);
+            Ring characterHead = body.get(0);
+
+            if (characterHead.posX == characterBody.posX && characterHead.posY == characterBody.posY) {
+                play = false;
+            }
+        }
+
+        Ring characterHead = body.get(0);
+        if (characterHead.posX < 10) {
+            play = false;
+        }
+        if (characterHead.posX > (Window.width - 20)) {
+            play = false;
+        }
+        if (characterHead.posY < 30) {
+            play = false;
+        }
+        if (characterHead.posX > (Window.height - 20)) {
+            play = false;
+        }
+    }
+
+    public void move() {
         int px, py;
-        for(int i = body.size()-1 ; i > 0 ; i--){
+        for (int i = body.size() - 1; i > 0; i--) {
             Ring r;
-            r = body.get(i-1);
+            r = body.get(i - 1);
             px = r.posX;
             py = r.posY;
             r = body.get(i);
@@ -115,19 +162,19 @@ public class Character extends JPanel{
             r.posY = py;
         }
         Ring r1 = body.get(0);
-        if(direction == 1){
+        if (direction == 1) {
             r1.posY -= 10;
         }
-        
-        if(direction == 2){
+
+        if (direction == 2) {
             r1.posX += 10;
         }
-        
-        if(direction == 3){
+
+        if (direction == 3) {
             r1.posY += 10;
         }
-        
-        if(direction == 4){
+
+        if (direction == 4) {
             r1.posX -= 10;
         }
     }
--- src/snake/Window.java
+++ src/snake/Window.java
@@ -5,51 +5,72 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class Window extends JFrame implements KeyListener {
+
     JPanel container = new JPanel();
-    
+
     public static int width = 400;
     public static int height = 400;
-    
+
     Character arbok;
-    
-    public Window(){
+
+    JLabel gameOver = new JLabel();
+
+    public Window() {
         setTitle("Jeu Snake 0.1 2017 (Alpha)");
         setSize(width, height);
         setResizable(false);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        
+
         container.setBackground(Color.WHITE);
-        
+
+        gameOver.setBounds((width / 2) - 40, 0, 100, 20);
+
         // On définit l'évènement sur la fenêtre (appui touche)
         addKeyListener(this);
-        
+
         // On ajoute le panel à la fenêtre courante
         setContentPane(container);
-        
+
         // On affiche la fenêtre
         setVisible(true);
-        
-        arbok = new Character(this.getGraphics(),4);
+
+        arbok = new Character(this.getGraphics(), 4);
     }
-    
-    public void startGame(){
+
+    public void startGame() {
         arbok.play();
+        gameOver();
+    }
+
+    public void gameOver() {
+        gameOver.setText("Perdu !");
+        container.add(gameOver);
+        container.repaint();
     }
 
     @Override
     public void keyTyped(KeyEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (e.getKeyCode() == KeyEvent.VK_UP && arbok.direction != 3) {
+            arbok.direction = 1;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT && arbok.direction != 4) {
+            arbok.direction = 2;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_DOWN && arbok.direction != 1) {
+            arbok.direction = 3;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_LEFT && arbok.direction != 2) {
+            arbok.direction = 4;
+        }
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
 }
